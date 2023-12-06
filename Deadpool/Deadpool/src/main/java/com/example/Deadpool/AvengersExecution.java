45cdee9f85064610d4d66e3dc74d0910df85c360
munwar999 <152465759+munwar999@users.noreply.github.com>
2023-12-06 13:10:24 +0530

    Add files via upload

---
+++ Deadpool/Deadpool/src/main/java/com/example/Deadpool/AvengersExecution.java
@@ -0,0 +1,16 @@
+package com.example.Deadpool;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+@RequestMapping(path="/go")
+public class AvengersExecution {
+	
+	@GetMapping(path="/boy")
+	public String check() {
+		return "I am Immortal";
+		
+	} 
+}
---
+++ Deadpool/Deadpool/src/main/java/com/example/Deadpool/DeadpoolApplication.java
@@ -0,0 +1,14 @@
+package com.example.Deadpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeadpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeadpoolApplication.class, args);
+		System.out.println("Deadpool is Best");
+	}
+
+}
